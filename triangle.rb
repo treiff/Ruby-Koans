6687f73551788500ccef5660441ca7dc77f93719
treiff <trevorreiff@gmail.com>
2014-08-21 21:25:33 -0400

    complete triangle

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a,b,c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else        :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
