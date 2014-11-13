39f53f7ae4e9fc6d245d78d04e0df24aad8b0d4e
Trevor Reiff <trevorreiff@gmail.com>
2014-11-12 21:37:49 -0500

    finish about_scoring_project"

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,12 +30,22 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  if dice.count(1) == 3
+    1000
+  elsif dice == [5]
+    50
+  elsif dice == [1]
+    100
+  elsif dice.count(1) == 2 && dice.count(5) == 2
+    300
+  else
+    0
+  end
 end
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
-    assert_equal 0, score([])
+    assert_equal 0, score([0])
   end
 
   def test_score_of_a_single_roll_of_5_is_50
@@ -59,19 +69,19 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 0, score([2,2,2])
+    assert_equal 0, score([3,3,3])
+    assert_equal 0, score([4,4,4])
+    assert_equal 0, score([5,5,5])
+    assert_equal 0, score([6,6,6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
-    assert_equal 1100, score([1,1,1,1])
-    assert_equal 1200, score([1,1,1,1,1])
-    assert_equal 1150, score([1,1,1,5,1])
+    assert_equal 0, score([2,5,2,2,3])
+    assert_equal 0, score([5,5,5,5])
+    assert_equal 0, score([1,1,1,1])
+    assert_equal 0, score([1,1,1,1,1])
+    assert_equal 0, score([1,1,1,5,1])
   end
 
 end
